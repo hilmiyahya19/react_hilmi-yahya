2eb81b16d9a8bfc88a12d504e898de2fb24c9a84
Hilmi Yahya <hilmiyahya199@gmail.com>
2024-03-17 14:37:21 +0700

    update tugas 17-3-2024

--- 13_Introduction_React/praktikum/vite/introduction-react/src/App.jsx
+++ 13_Introduction_React/praktikum/vite/introduction-react/src/App.jsx
@@ -1,17 +1,17 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import Navbar from './components/Navbar';
+// import Navbar from './components/Navbar';
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Navbar></Navbar>
-      <h1>hello</h1>
-      {/* <div>
+      {/* <Navbar></Navbar>
+      <h1>hello</h1> */}
+      <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -30,7 +30,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p> */}
+      </p>
     </>
   )
 }
