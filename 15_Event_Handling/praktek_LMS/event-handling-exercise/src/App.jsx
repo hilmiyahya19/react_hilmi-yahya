d869e3165eb1c4656619e2de46452639de5ed77c
Hilmi Yahya <hilmiyahya199@gmail.com>
2024-03-23 09:39:22 +0700

    update tugas 23-3-2024

--- 15_Event_Handling/praktek_LMS/event-handling-exercise/src/App.jsx
+++ 15_Event_Handling/praktek_LMS/event-handling-exercise/src/App.jsx
@@ -1,36 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Button, Form } from 'react-bootstrap';
 import './App.css'
-// import Navbar from './components/Navbar';
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [copiedText, setCopiedText] = useState('');
+  const handleCopy = () => {
+    setCopiedText('Teks Berhasil di Salin')
+    navigator.clipboard.writeText('Teks Berhasil di Salin')
+    console.log('Teks Berhasil di Salin');
+  }
+
+  const [name, setName] = useState('');
+  const handleChangeName = (e) => {
+    setName(e.target.value);
+  }
+
+  const [email, setEmail] = useState('');
+  const handleChangeEmail = (e) => {
+    setEmail(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Name: ', name);
+    console.log('Email: ', email);
+  }
+
+  const [position, setPosition] = useState({ x:0, y:0})
+  const handleMouseMove = (e) => {
+    setPosition({x:e.clientX, y:e.clientY})
+  }
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    alert('anda menekan bagian ini')
+  }
+
+  const handleDelete = (id) => {
+    alert('ID yang anda ingin hapus adalah ' + id)
+
+  }
 
   return (
     <>
-      {/* <Navbar></Navbar>
-      <h1>hello</h1> */}
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <Form.Control className='mb-2' type='text' value={copiedText} readOnly/>
+        <Button varian='primary' onClick={handleCopy}>Salin Text</Button>
+        <hr />
+        <Form onSubmit={handleSubmit}>
+          <Form.Label>Name:</Form.Label>
+          <Form.Control type='text' id='name' value={name} onChange={handleChangeName}/>
+          <br />
+          <Form.Label>Email:</Form.Label>
+          <Form.Control type='email' id='email' value={email} onChange={handleChangeEmail}/>
+          <br />
+          <Button type='submit'>Submit</Button>
+        </Form>
+        <hr />
+        <div onMouseMove={handleMouseMove}>
+          <p>Gerakkan mouse Anda di area ini</p>
+          <p>Posisi X : {position.x}</p>
+          <p>Posisi Y : {position.y}</p>
+        </div>
+        <hr />
+        <div onClick={handleClick}>
+            <p>Coba anda klik di area ini</p>
+        </div>
+        <hr />
+        <div>
+          <div className='mt-2'>
+            <Button variant='danger' onClick={() => handleDelete(1)}>Tombol Delete dengan ID 1</Button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
