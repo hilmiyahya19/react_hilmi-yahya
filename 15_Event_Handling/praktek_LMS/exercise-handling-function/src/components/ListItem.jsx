d6ba1e1e1c6677d9b359e54e1ec0afc1391b192b
Hilmi Yahya <hilmiyahya199@gmail.com>
2024-03-23 10:24:36 +0700

    update tugas 23-3-2024

---
+++ 15_Event_Handling/praktek_LMS/exercise-handling-function/src/components/ListItem.jsx
@@ -0,0 +1,17 @@
+
+function ListItem({item, hapusPengunjung}) {
+  return (
+    <>
+        <tr>
+            <td>{item.nama}</td>
+            <td>{item.umur}</td>
+            <td>{item.jenisKelamin}</td>
+            <td>
+                <button onClick={() => {hapusPengunjung(item.id)}}>Hapus</button>
+            </td>
+        </tr>
+    </>
+  )
+}
+
+export default ListItem;
