d8c6a84832d8ac61a7662cab749190412725cabb
Hilmi Yahya <hilmiyahya199@gmail.com>
2024-03-29 14:50:46 +0700

    exercise-react-routing

--- 17_React_Routing/praktek_LMS/exercise-react-routing/src/App.jsx
+++ 17_React_Routing/praktek_LMS/exercise-react-routing/src/App.jsx
@@ -1,38 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-// import Navbar from './components/Navbar';
+import { Link, Outlet, Route, Routes } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      {/* <Navbar></Navbar>
-      <h1>hello</h1> */}
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>contoh dasar</h1>
+      {/* <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint, illo dolorum rerum ullam quibusdam provident? Omnis numquam dolores quidem adipisci?</p> */}
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route path="/home" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+        </Route>
+      </Routes>
     </>
   )
 }
 
 export default App;
+
+function Layout() {
+  return(
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>beranda</Link>
+          </li>
+          <li>
+            <Link to="/home">home</Link>
+          </li>
+          <li>
+            <Link to="/about">about</Link>
+          </li>
+        </ul>
+      </nav>
+      <hr />
+      <Outlet/>
+    </div>
+  )
+}
+
+function Home() {
+  return(
+    <>
+      <div>
+        <h1>Home</h1>
+      </div>
+    </>
+  )
+}
+function About() {
+  return(
+    <>
+      <div>
+        <h1>About</h1>
+      </div>
+    </>
+  )
+}
