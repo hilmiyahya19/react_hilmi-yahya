f8f055a2f030acc03897d53f5d089e92de4d40a7
Hilmi Yahya <hilmiyahya199@gmail.com>
2024-03-30 23:19:55 +0700

    update praktikum

--- 17_React_Routing/praktikum/project/src/App.jsx
+++ 17_React_Routing/praktikum/project/src/App.jsx
@@ -2,28 +2,47 @@ import { Route, Routes } from "react-router-dom";
 import Layout from "./Layout/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
-// import Products from "./pages/Products";
 import Contacts from "./pages/Contacts";
+// import Products from "./pages/Products";
 import { ThemeProvider } from "styled-components";
 import theme from "./utils/constants/theme";
 import DetailProduct from "./components/DetailProduct/DetailProduct";
 import CreateProduct from "./components/CreateProduct/CreateProduct";
 import { useState } from "react";
 import productData from "./components/CreateProduct/productData";
+import Login from "./components/Login/Login";
+import NotFound from "./components/NotFound/NotFound";
 
 function App() {
   const [data, setData] = useState([]);
+
+  const [isLoggedIn, setLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    console.log('Login successful');
+    setLoggedIn(true); // Setelah login berhasil, ubah state isLoggedIn menjadi true
+  };
 
   return (
     <div> 
       <ThemeProvider theme={theme}>
         <Layout>
           <Routes>
-            <Route path="/" element={<Home/>}></Route>
-            <Route path="/products" element={<CreateProduct data={data} setData={setData}/>}></Route>
-            <Route path="/products/detail-product/:id" element={<DetailProduct productData={productData}/>}></Route>
-            <Route path="/about" element={<About/>}></Route>
-            <Route path="/contacts" element={<Contacts/>}></Route>
+            {/* Route untuk Login */}
+            {!isLoggedIn && <Route path="/" element={<Login onLogin={handleLogin} />} />}
+            {/* Route untuk Home dan rute yang memerlukan login */}
+            {isLoggedIn && (
+              <>
+                <Route path="/" element={<Home />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/products" element={<CreateProduct data={data} setData={setData} />} />
+                <Route path="/products/detail-product/:id" element={<DetailProduct productData={productData} />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contacts" element={<Contacts />} />
+              </>
+            )}
+            {/* Route untuk NotFound */}
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </Layout>
       </ThemeProvider>
@@ -32,4 +51,7 @@ function App() {
 }
 
 export default App;
+
+
+
 
---
+++ 17_React_Routing/praktikum/project/src/components/Login/Login.jsx
@@ -0,0 +1,68 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types'; 
+
+const Login = ({ onLogin }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [loginError, setLoginError] = useState(false);
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Di sini Anda dapat menambahkan logika untuk memeriksa kredensial, misalnya dengan menyimpannya di state atau memanggil API
+    if (username === 'admin' && password === 'admin') {
+      onLogin(); // Panggil fungsi onLogin jika kredensial benar
+    } else {
+      setLoginError(true);
+    }
+  };
+
+  return (
+    <div className="flex h-screen justify-center items-center bg-gray-200">
+      <form onSubmit={handleSubmit} className="bg-white p-8 rounded-lg shadow-md">
+        <h2 className="text-2xl font-semibold mb-4">Login</h2>
+        {loginError && <p className="text-red-500 mb-2">Invalid username or password</p>}
+        <div className="mb-4">
+          <label htmlFor="username" className="block text-gray-700 font-semibold mb-2">Username</label>
+          <input
+            type="text"
+            id="username"
+            className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+            placeholder="Enter your username"
+            value={username}
+            onChange={handleUsernameChange}
+            required
+          />
+        </div>
+        <div className="mb-6">
+          <label htmlFor="password" className="block text-gray-700 font-semibold mb-2">Password</label>
+          <input
+            type="password"
+            id="password"
+            className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+            placeholder="Enter your password"
+            value={password}
+            onChange={handlePasswordChange}
+            required
+          />
+        </div>
+          <button type="submit" className="w-full bg-blue-500 text-white py-2 px-4 rounded-md focus:outline-none hover:bg-blue-600 focus:bg-blue-600">
+            Login
+          </button>
+      </form>
+    </div>
+  );
+};
+
+Login.propTypes = {
+  onLogin: PropTypes.func.isRequired, 
+};
+
+export default Login;
