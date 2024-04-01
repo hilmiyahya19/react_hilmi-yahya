cfdf2ad6a4d274cc02080300ca0ce6a4c008b94d
Hilmi Yahya <hilmiyahya199@gmail.com>
2024-04-02 01:23:30 +0700

    Update Welcome.jsx

--- 17_React_Routing/praktikum/project/src/components/Welcome/Welcome.jsx
+++ 17_React_Routing/praktikum/project/src/components/Welcome/Welcome.jsx
@@ -4,7 +4,7 @@ function Welcome() {
   return (
     <div className="min-h-screen flex justify-center items-center">
       <div className="max-w-4xl mx-auto p-8 bg-white shadow-lg rounded-lg">
-        <h1 className="text-4xl font-bold text-center mb-8">Welcome to Our Website</h1>
+        <h1 className="text-4xl font-bold text-center mb-8">Welcome to My Website</h1>
         <p className="text-lg text-gray-700 mb-8">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed imperdiet libero vel justo commodo dapibus.</p>
         <div className="flex justify-center">
           <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline">Sign Up</button>
