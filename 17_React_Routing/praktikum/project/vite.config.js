ce102f1960921d078b3484dd6ee3606e41afd4de
Hilmi Yahya <hilmiyahya199@gmail.com>
2024-04-03 15:20:18 +0700

    update

--- 17_React_Routing/praktikum/project/vite.config.js
+++ 17_React_Routing/praktikum/project/vite.config.js
@@ -1,19 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react', 'react-dom', 'react-router-dom'], // tambahkan 'react' dan 'react-dom' sebagai modul eksternal juga
-      output: {
-        globals: {
-          'react': 'React', // definisikan variabel global 'react'
-          'react-dom': 'ReactDOM', // definisikan variabel global 'react-dom'
-          'react-router-dom': 'ReactRouterDOM' // definisikan variabel global 'react-router-dom'
-        }
-      }
-    }
-  }
-});
+})
