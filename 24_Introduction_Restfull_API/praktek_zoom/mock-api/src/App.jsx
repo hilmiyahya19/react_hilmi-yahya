d4f24644e2291ddd84b8dc8b23f4bc98ac702e27
Hilmi Yahya <hilmiyahya199@gmail.com>
2024-04-17 17:07:11 +0700

    praktek-zoom

--- 24_Introduction_Restfull_API/praktek_zoom/mock-api/src/App.jsx
+++ 24_Introduction_Restfull_API/praktek_zoom/mock-api/src/App.jsx
@@ -1,38 +1,103 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-// import Navbar from './components/Navbar';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([]);
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  const [nameEdit, setNameEdit] = useState('');
+  const [ageEdit, setAgeEdit] = useState('');
+
+  async function fetchData() {
+    const response = await axios.get(`https://660fae7f356b87a55c520818.mockapi.io/users`);
+    setData(response.data);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleClick = async (id) => {
+    console.log(`Deleting user with id: ${id}`);
+    await axios.delete(`https://660fae7f356b87a55c520818.mockapi.io/users/${id}`);
+    fetchData();
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log('Adding user:', name, age);
+    await axios.post('https://660fae7f356b87a55c520818.mockapi.io/users', { name, age });
+    setName('');
+    setAge('');
+    fetchData();
+  };
+
+  const handleSubmitEdit = async (e, id) => {
+    e.preventDefault();
+    console.log('Editing user:', nameEdit, ageEdit, id);
+    await axios.put(`https://660fae7f356b87a55c520818.mockapi.io/users/${id}`, { name: nameEdit, age: ageEdit });
+    setNameEdit('');
+    setAgeEdit('');
+    fetchData();
+  };
 
   return (
-    <>
-      {/* <Navbar></Navbar>
-      <h1>hello</h1> */}
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl mb-4">Fetch Data with Axios</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col mb-4">
+        <input 
+          type="text" 
+          placeholder="Name" 
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          className="input input-bordered mb-2 md:w-auto border"
+        />
+        <input 
+          type="number" 
+          placeholder="Age" 
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+          className="input input-bordered mb-2 md:w-auto border"
+        />
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Submit
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      </form>
+      {data.map((user) => (
+        <div key={user.id} className="mb-4 p-4 border rounded">
+          <p className="mt-2">Name: {user.name}</p>
+          <p>Age: {user.age}</p>
+          <img src={user.image} alt="User" className="mt-2 mb-4 rounded" />
+          <button 
+            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+            onClick={() => handleClick(user.id)}
+          >
+            Delete
+          </button>
+          <form onSubmit={(e) => handleSubmitEdit(e, user.id)} className="mt-4">
+            <input 
+              type="text" 
+              placeholder="New name" 
+              value={nameEdit}
+              onChange={(e) => setNameEdit(e.target.value)}
+              className="input input-bordered mb-2 md:w-auto border"
+            />
+            <input 
+              type="number" 
+              placeholder="New age" 
+              value={ageEdit}
+              onChange={(e) => setAgeEdit(e.target.value)}
+              className="input input-bordered mb-2 md:w-auto border"
+            />
+            <br />
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Submit
+            </button>
+          </form>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
