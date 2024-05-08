893cf02bd9b8189c4c927f3949493566c93d76fe
Hilmi Yahya <hilmiyahya199@gmail.com>
2024-05-08 07:27:21 +0700

    praktek-lms

---
+++ 26_Authentication_in_React/praktek_LMS/authentication/src/components/index.jsx
@@ -0,0 +1,5 @@
+// src/components/index.jsx
+export {default as Login} from "./Login/Login"
+export {default as Home} from "./Home/Home"
+export {default as Register} from "./Register/Register"
+export {default as Dashboard} from "./Dashboard/Dashboard"
